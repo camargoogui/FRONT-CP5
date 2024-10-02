5a66208e047b3762b2e841bd80d7c619364a4daa
icaroalb1 <icaroalb1senac@gmail.com>
2024-10-02 15:53:55 -0300

    Atualizando pagina e css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import Link from "next/link";
- 
+import "./globals.css"; 
+
 export const metadata: Metadata = {
   title: {
     template: "Worlds In Collision",
