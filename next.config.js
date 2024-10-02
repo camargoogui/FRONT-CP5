2d581f681bc63ada327875bdc2abee543735962b
icaroalb1 <icaroalb1senac@gmail.com>
2024-10-02 16:35:08 -0300

    att layout e page css

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['images-assets.nasa.gov'], // Adiciona o domínio externo da NASA para carregar imagens
+    },
+  };
+  
+  module.exports = nextConfig;
+  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import Link from "next/link";
-import "./globals.css"; 
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: {
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     default: "Worlds In Collision",
   },
 };
- 
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,15 +16,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>
-        <div>
-          <nav>
-            <Link href="/">Introdução</Link>
-            <Link href="/orbitas-planetarias">Órbitas Planetárias e Instabilidade</Link>
-            <Link href="/cronologia-eventos">Cronologia dos Eventos</Link>
-            <Link href="/calculo">Cálculo</Link>
+      <body className="antialiased h-screen w-full bg-gradient-to-r from-black to-indigo-900 text-slate-200">
+        <div className="flex flex-col items-center border-b-4 border-cyan-500 shadow-lg">
+          <nav className="w-full flex justify-center gap-8 py-6 text-xl text-cyan-400 uppercase tracking-wide font-mono">
+            <Link href="/" className="hover:text-cyan-200 transition duration-300">Introdução</Link>
+            <Link href="/orbitas-planetarias" className="hover:text-cyan-200 transition duration-300">Órbitas Planetárias e Instabilidade</Link>
+            <Link href="/cronologia-eventos" className="hover:text-cyan-200 transition duration-300">Cronologia dos Eventos</Link>
+            <Link href="/calculo" className="hover:text-cyan-200 transition duration-300">Cálculo</Link>
           </nav>
-          <main>
+          <main className="w-full px-6 py-4">
             {children}
           </main>
         </div>
