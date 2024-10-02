b27a596f84d7ea51be6d282f8173dda2e5f53b62
biancavitoria15 <bianca1.vitoria@gmail.com>
2024-10-02 15:26:56 -0300

    Atualizando Orbitas e Not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,17 +3,20 @@ import imgErro from '/public/img/404.jpg';
 
 export default function NotFound() {
   return (
-    <div>
+    <div className="h-screen flex flex-col justify-center items-center bg-slate-900 text-slate-200">
+      
       <Image
         src={imgErro}
         alt="Erro 404"
         width={500}
         height={500}
+        className="rounded-lg shadow-lg"
       />
-      
-      <div>
-        <h1>Página Não Encontrada</h1>
-        <p>
+
+    
+      <div className="mt-8 text-center">
+        <h1 className="text-4xl font-bold text-cyan-400">Página Não Encontrada</h1>
+        <p className="text-lg text-slate-300 mt-4">
           A página que você está procurando não existe. Talvez o URL esteja incorreto ou a página tenha sido removida.
         </p>
       </div>
--- src/app/orbitas-planetarias/page.tsx
+++ src/app/orbitas-planetarias/page.tsx
@@ -3,13 +3,13 @@ import Venus from '/public/img/venus.jpg';
 
 export default function CosmicImpact() {
   return (
-    <section className="">
-      <h1 className="">Órbitas Planetárias e Instabilidade</h1>
+    <section className="flex flex-col items-center py-8 px-14 gap-12 bg-slate-900 text-slate-50">
+      <h1 className="cosmic-title">Órbitas Planetárias e Instabilidade</h1>
 
       
-      <div className="">
-        <div className="">
-          <p className="">
+      <div className="cosmic-container">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-6">
+          <p className="text-lg text-justify md:w-1/2">
             Velikovsky sugeriu que Vênus, Marte e outros corpos celestes poderiam ter passado perto da Terra, perturbando
             as órbitas planetárias de forma significativa. Isso implicava em uma instabilidade no sistema solar que não era
             compatível com os cálculos newtonianos de órbitas estáveis a longo prazo.
@@ -21,7 +21,7 @@ export default function CosmicImpact() {
             alt="Imagem de Vênus"
             width={500}
             height={300}
-            className=""
+            className="rounded-xl shadow-lg max-w-full"
           />
         </div>
       </div>
