4b2b739c4c232a687fcf4808c8316be99dff9035
icaroalb1 <icaroalb1senac@gmail.com>
2024-10-02 15:57:21 -0300

    atualizando class cronologia

--- src/app/cronologia-eventos/page.tsx
+++ src/app/cronologia-eventos/page.tsx
@@ -3,12 +3,12 @@ import EventosAstronomicos from '/public/img/eventos-astronomicos.jpg';
 
 export default function PlanetaryCollisions() {
   return (
-    <section className="">
-      <h1 className="">Cronologia dos Eventos</h1>
+    <section className="flex flex-col items-center py-8 px-14 gap-12 bg-slate-900 text-slate-50">
+      <h1 className="cosmic-title">Cronologia dos Eventos</h1>
 
-      <div className="">
-        <div className="">
-          <p className="">
+      <div className="cosmic-container">
+        <div className="flex flex-col md:flex-row items-center justify-center gap-6">
+          <p className="text-lg text-justify md:w-1/2">
             Ele questionou a cronologia aceita de eventos históricos e astronômicos,
             propondo que certos eventos catastróficos ocorreram em tempos muito mais
             recentes do que a ciência convencional sugere. A ideia de que planetas
@@ -22,7 +22,7 @@ export default function PlanetaryCollisions() {
             alt="Imagem de eventos astronômicos"
             width={800}
             height={600}
-            className=""
+            className="rounded-xl shadow-lg max-w-full"
           />
         </div>
       </div>
