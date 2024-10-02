256c3778a2c14d141a31916116f0a37ad602944d
biancavitoria15 <bianca1.vitoria@gmail.com>
2024-10-02 15:35:14 -0300

    Criando API

---
+++ src/app/api/nasa/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+    const apiKey = process.env.jfJZesx62uPt8DtewrBwVdxaB59u2r4fk4PuYVSX;
+    const response = await fetch(https://api.nasa.gov/planetary/apod?api_key=${apiKey});
+    const data = await response.json();
+    return NextResponse.json(data);
+}
+
